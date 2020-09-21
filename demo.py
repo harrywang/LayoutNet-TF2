e037f9ce8e8aec15350248d51a3acc68b0667b2b
Harry Wang <harryjwang@gmail.com>
2020-09-21 12:09:01 -0400

    added testing images and updated requirements.txt

--- demo.py
+++ demo.py
@@ -51,18 +51,28 @@ class LayoutNetDemo:
 
 
 if __name__ == '__main__':
+    print('loading the checkpoint...')
     demo = LayoutNetDemo(checkpoint_path='./checkpoints/ckpt-300')
 
     category = 'food'
     text_ratio = 0.5
     image_ratio = 0.5
-    image_path = ['./test1.jpg', './test2.jpg']
+    image_path1 = ['./demo/food.jpg', './demo/wine.jpg']
+    image_path2 = ['./demo/fashion.jpg']  # not food related
     keywords_list = ['Taste', 'wine', 'restaurant', 'fruit', 'market']
 
     # generate random latent variable
     z = np.random.normal(0.0, 1.0, size=(1, config.z_dim)).astype(np.float32)
 
     # generate result
-    image = demo.generate(category, text_ratio, image_ratio, image_path,
+    print('generating demo 1')
+    image = demo.generate(category, text_ratio, image_ratio, image_path1,
                           keywords_list, z)
-    image.save('./demo.png')
+    image.save('./demo/demo1.png')
+
+    # generate result
+    print('generating demo 2')
+    image = demo.generate(category, text_ratio, image_ratio, image_path2,
+                          keywords_list, z)
+    image.save('./demp/demo2.png')
+    
